da0c87507c21a154690c7d512ea01237bc58f7e7
Dipakkamble <kambledipak700@gmail.com>
2023-02-09 18:53:29 +0530

    [Dipak] Modified add contact inside addressbook

--- AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBook.cs
@@ -8,6 +8,7 @@ namespace AddressBookSystem
 {
     internal class AddressBook
     {
+        List<Contact> contactList = new List<Contact>();
         public void CreateContact()
         {
             Contact contact = new Contact();
@@ -36,5 +37,45 @@ namespace AddressBookSystem
             contact.email = Console.ReadLine();
             Console.WriteLine("\nContact Added Succesfully....!\n");
         }
+        public void AddContact()
+        {
+            Contact contact = new Contact();
+            Console.Write("Enter First Name : ");
+            contact.fName = Console.ReadLine();
+
+            Console.Write("Enter Last Name : ");
+            contact.lName = Console.ReadLine();
+
+            Console.Write("Enter Address : ");
+            contact.address = Console.ReadLine();
+
+            Console.Write("Enter City Name : ");
+            contact.city = Console.ReadLine();
+
+            Console.Write("Enter state Name : ");
+            contact.state = Console.ReadLine();
+
+            Console.Write("Enter ZIP Code : ");
+            contact.zipCode = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Phone Number : ");
+            contact.phoneNumber = Convert.ToInt64(Console.ReadLine());
+
+            Console.Write("Enter Email Id : ");
+            contact.email = Console.ReadLine();
+            contactList.Add(contact);
+            Console.WriteLine("\nContact Added Succesfully....!\n");
+
+        }
+            public void DisplayContact()
+            {
+                AddContact();
+                foreach (Contact data in contactList)
+                {
+                    Console.WriteLine("Contact Details");
+                    Console.WriteLine("1.Name: " + data.fName + " " + data.lName + "\n 2.Address: " + data.address + "\n 3.City: " + data.city + "\n 4.State: " + data.state + "\n 5.Zipcode: " + data.zipCode + " 6.EMAIL:  " + data.email + "\n 7.PhoneNumber: " + data.phoneNumber);
+                }
+            }
+        
     }
 }
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -12,8 +12,23 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to address book system");
-            AddressBook addressBook= new AddressBook();
-            addressBook.CreateContact();
+            Console.WriteLine("Enter the number from below option");
+            Console.WriteLine("1:Create conatct\n2:Add contact\n3:Display contact");
+            int option =Convert.ToInt32(Console.ReadLine());
+            AddressBook addressBook = new AddressBook();
+            switch (option) 
+            {
+                case 1:                    
+                    addressBook.CreateContact();
+                    break;
+                case 2:
+                    addressBook.AddContact();
+                    break;
+                case 3:
+                    addressBook.DisplayContact();
+                    break;
+            }
+            
             Console.ReadLine();
         }
     }
